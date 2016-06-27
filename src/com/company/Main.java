ed13cd5c562f2101e2ef4b1716940bef94db3030
Velando <philipnicolas93@hotmail.com>
2016-06-27 22:21:18 +0200

    Removed imports

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,10 +1,5 @@
 package com.company;
 
-import dnl.utils.text.table.TextTable;
-
-import java.util.List;
-import java.util.Scanner;
-
 public class Main {
 
     public static void main(String[] args) {
